0efcd5bce43f60f3dfcacb8bae19abadd712925a
Nahom Assefa <nzenebe@gmail.com>
2021-12-25 14:06:11 -0600

    adjustment

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 // Establishing port/routes, and access to packages
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const noteRoutes = require('./routes/noteRoutes');
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', noteRoutes);
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", noteRoutes);
 
-app.listen(PORT, () => console.log(`API server now on port http://localhost:${PORT}!`));
+app.listen(PORT, () => {
+  console.log(`API server now on port http://localhost:${PORT}!`);
+});
