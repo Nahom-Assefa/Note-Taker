57e9917c3679b7df8266916d0893565f22661408
Nahom Assefa <nzenebe@gmail.com>
2021-12-25 13:11:03 -0600

    adjustment

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const noteRoutes = require('./routes/noteRoutes');
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
